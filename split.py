9d1d9d2479660885b68145d716b72f84a5c7cb15
Lucas Henrique <lucashenriqueslima@outlook.com>
2022-07-04 18:30:19 -0300

    selecionando 2 números

--- split.py
+++ split.py
@@ -21,29 +21,31 @@ def search_for_contact(browser_instance, contact, data):
     time.sleep(2)
     input_seacrh.send_keys(contact)
 
-    time.sleep(2)
+    time.sleep(4)
 
     if browser_instance.find_elements(By.CSS_SELECTOR ,"div[style='height: 49px; width: 49px;']") :
         
         input_seacrh.send_keys(Keys.ENTER)
         print("Encontrou o contato")
         return True
-
-    contac_next_try = contact.split(" ");
-    contact = contac_next_try[0] + " " + contac_next_try[1][1:];
-    input_seacrh.send_keys(Keys.CONTROL, "a")
-    input_seacrh.send_keys(Keys.BACKSPACE)
-    input_seacrh.click()
-    time.sleep(2)
-    input_seacrh.send_keys(contact) 
+    
+    if len(contact.split(" ")) > 1:
+        contac_next_try = contact.split(" ");
+        contact = contac_next_try[0] + " " + contac_next_try[1][1:];
+        input_seacrh.send_keys(Keys.CONTROL, "a")
+        input_seacrh.send_keys(Keys.BACKSPACE)
+        input_seacrh.click()
+        time.sleep(1)
+        input_seacrh.send_keys(contact) 
+        time.sleep(4)
     
     if browser_instance.find_elements(By.CSS_SELECTOR ,"div[style='height: 49px; width: 49px;']"):
         input_seacrh.send_keys(Keys.ENTER)
-        print("Encontrou o contato")
+        
         return True
 
-    # response = requests.post("https://www.studiomfotografia.com.br/api/scripts/save-regua-cobranca-erro?passwd=a)()8***0--asf", {"id_duplicata_fatura": data['id_item'], "nome_formando": data['nome_formando'], "cod_formando": data['id'], "telefone": data['telefone'], "parcela": data['parcela_atual'], "motivo": "Número não encontrado"})
-    # print(response.text)
+    response = requests.post("https://www.studiomfotografia.com.br/api/scripts/save-regua-cobranca-erro?passwd=a)()8***0--asf", {"id_duplicata_fatura": data['id_item'], "nome_formando": data['nome_formando'], "cod_formando": data['id'], "telefone": data['telefone'], "parcela": data['parcela_atual'], "motivo": "Número não encontrado"})
+    print(response.text)
     print("Contact not found")
     return False
 
@@ -83,8 +85,6 @@ def do_scrap(contact, message, need_to_send_document, document_path):
 datas = requests.get("https://www.studiomfotografia.com.br/api/scripts/get-regua-cobranca-to-script?passwd=a)()8***0--asf").json()
 base_dir = "C:\\Users\\Studio M\\Documents\\"
 
-print(datas)
-
 # for data in datas:
 
 #     url_to_get_pdf = data['boleto_wpp']
@@ -110,13 +110,15 @@ browser = webdriver.Chrome(service=servico, chrome_options=options)
 base_dir = "C:\\Users\\Studio M\\Documents\\"
 
 browser.get("https://web.whatsapp.com/")
-
+input_seacrh = WebDriverWait(browser, 1000).until(EC.element_to_be_clickable((By.CLASS_NAME, "selectable-text")))
+input_seacrh.click()
+time.sleep(2)
+input_seacrh.send_keys("teste")
 for data in datas:
     if(search_for_contact(browser, data['telefone'], data) == True):
-        print("fim")
         time.sleep(2)
         send_message(browser, data['mensagem_wpp'])
         send_document(browser, base_dir + data['id_item'] + f".{'html' if data['link'] != None else 'pdf'}")
+time.sleep(1000)
 
-browser.close()
 
